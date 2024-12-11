66dab5abc9761a4847adc00374815c9da7f8cb30
vararais <varadigmaisnann@gmail.com>
2024-12-11 21:15:06 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Route for the home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route for form submission
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    return render_template('index.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
